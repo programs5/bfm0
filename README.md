4790390c8f57758a97ec637fbb7f9dfecd577fd9
pavel.malygin_work <Pavel.Malygin@gmail.com>
2018-07-04 12:14:22 +0300

    6

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# bfm0 Test
+#
+# bfm0 Реализация задания уровня Medium
+#
+
+в директории encripter созданы 3 скрипта:
+ - genkeys.js
+ - gensig.js
+ - verifysig.js
+ 
+1. "node genkeys.js" -  создает на диске ключевую пару private.key и public.key
+ 
+2. "node gensig.js <file> <private key>" - для указанного файла высчитывает хешь и шифрует его секретным ключом, сохраняет полученную подпись в файле с расширением dig.
+
+3. "node verifysig.js <file> <signature> <public key>" - проверяет совпадение подписи для указанного файла с использованием открытого ключа.
--- encripter/index.js
+++ encripter/index.js
@@ -1,8 +1,12 @@
 const fs = require("fs");
 const secp256k1 = require('secp256k1');
 
-var path = "test1.txt";
+var file = "test1.txt";
 var fileKey = "private.key";
+
+const blockSize = 32;
+
+let encriptText = new Buffer.alloc(0);
 
 // загружаем приватный ключ
 fs.readFile(fileKey, function (err, data) {
@@ -11,23 +15,29 @@ fs.readFile(fileKey, function (err, data) {
     }
     var privateKey =  data;
 
-    // bufferSize
-    var stream = new fs.createReadStream(path, {highWaterMark: 32});
+    var stream = new fs.createReadStream(file, {highWaterMark: blockSize});
 
+    // readable
     stream.on('readable', function() {
       var data = stream.read();
 
       if (data != null) {
-        //console.log(data);
-        const sig = secp256k1.sign(data, privateKey).signature;
-        console.log(sig);
+         if(data.length < blockSize) {
+            data = Buffer.concat([data,  new Buffer.alloc(blockSize - data.length)]);
+         }
+         const sig = secp256k1.sign(data, privateKey).signature;
+         encriptText = Buffer.concat([encriptText, sig]);
       }
     });
+
+    // end
+    stream.on('end', function() {
+      // записываем зашифрованный текст в файл
+      fs.writeFile(file + ".enc", encriptText, function(err) {
+        if(err) {
+          return console.log(err);
+        }
+        console.log(`Encripted text saved in "${file}.enc"`);
+      });
+    });
   });
-
-
-
-
-//stream.on('end', function() {
-//  console.log('END!');
-//});
